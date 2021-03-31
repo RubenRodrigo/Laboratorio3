908cb0aba6d8fe9319bc9b297cfd943ec68bc677
Ruben Rodrigo <ruben.chani@tecsup.edu.pe>
2021-03-30 20:57:38 -0500

    conditional render

--- vote/src/Statistics.js
+++ vote/src/Statistics.js
@@ -3,12 +3,18 @@ export const Statistics = (props) => {
   return (
     <div>
       <h1>Statistics</h1>
-      <p>Good: <span>{good}</span></p>
-      <p>Neutral: <span>{neutral}</span></p>
-      <p>Bad: <span>{bad}</span></p>
-      <p>All: <span>{all}</span></p>
-      <p>Average: <span>{average}</span></p>
-      <p>Positive: <span>{positive}</span>%</p>
+      {good || neutral || bad ?
+        <>
+          <p>Good: <span>{good}</span></p>
+          <p>Neutral: <span>{neutral}</span></p>
+          <p>Bad: <span>{bad}</span></p>
+          <p>All: <span>{all}</span></p>
+          <p>Average: <span>{average}</span></p>
+          <p>Positive: <span>{positive}</span>%</p>
+        </>
+      :
+        <h3>No feedback given</h3>
+      }
     </div>
   )
 }
